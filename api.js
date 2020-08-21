329c39b9214734480e0794d162e971365fa7d2fb
Mowafag <mmowfug@yahoo.com>
2020-08-21 10:58:56 +0200

    third commit

--- api.js
+++ api.js
@@ -1,6 +1,7 @@
   
 const express = require('express');
 const MongoClient = require('mongodb').MongoClient;
+const { ObjectID, ObjectId } = require('bson');
 const app = express()
 
 app.use (express.json())
@@ -31,10 +32,10 @@ async function group(){
     .put('/updateStudents', async (req,res) => {
       if(req.body.reset == false){
         for(const obj in req.body.objects){
-          await students.updateOne(obj, {$set: {selected: true}})
+          await students.updateOne(req.body.objects[obj], {$set: {selected: true}})
         }
       } else {
-        await students.updateMany({selected: true}, {$set: {selected: false}})
+        await students.updateMany({}, {$set: {selected: false}})
       }  
       res.send()
     })
